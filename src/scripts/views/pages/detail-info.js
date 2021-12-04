1f16ad8c1fea8ea49832281535c553c018b5a08f
Nurfadhilah <nurfadhilahalang480@gmail.com>
2021-12-04 19:18:05 +0800

    update request API

--- src/scripts/views/pages/detail-info.js
+++ src/scripts/views/pages/detail-info.js
@@ -1,12 +1,42 @@
+import FypDbSource from '../../data/fyp-source';
+
 const DetailInfo = {
   async render() {
     return `
-        <h2>DetailInfo page</h2>
-      `;
+    <section id="detail-item" class="mt-5">
+      <div class="container pt-5">
+          <div class="row text-center mb-4">
+              <div class="col">
+                  <h2>Detail Info</h2>
+              </div>
+          </div>
+          <div class="row mb-4 p-4 bg-detail">
+              <div class="col">
+                  <div class="card">
+                      <img src="img/img1.jpg" class="card-img-top" alt="...">
+                      <div class="card-body text-center">
+                          <h5 class="card-title">Belajar Dasar Pemrograman Web</h5>
+                          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                          <p class="card-text text-secondary">Dibuat Oleh Dianah</p>
+                          <p class="card-text"><a href="#" class="link">link pendaftaran</a></p>
+                      </div>
+                  </div>
+              </div>
+          </div>
+          <div class="row text-center comentar">
+              <div class="col">
+                  <h5 class="card-title"><a href="#" class="link">Lihat Komentar</a></h5>
+              </div>
+          </div>
+      </div>
+    </section>
+    `;
   },
 
   async afterRender() {
-    // Fungsi ini akan dipanggil setelah render()
+    const url = UrlParser.parseActiveUrlWithoutCombiner();
+    const detailInfo = await FypDbSource.IdetailInfoInfo(url.id);
+    console.log(detailInfo);
   },
 };
 
--- src/scripts/views/pages/list-info.js
+++ src/scripts/views/pages/list-info.js
@@ -1,12 +1,53 @@
+import FypDbSource from '../../data/fyp-source';
+
 const ListInfo = {
   async render() {
     return `
-        <h2>ListInfo page</h2>
+    <section id="list-item" class="mt-5">
+      <div class="container pt-5">
+        <div class="row text-center mb-4">
+            <div class="col">
+                <h2>Web Developer</h2>
+            </div>
+        </div>
+        <div class="row list-item mb-4">
+            <div class="col align-self-center">
+                <img src="https://media.istockphoto.com/photos/pond-at-waterline-picture-id1035285964?k=6&m=1035285964&s=612x612&w=0&h=yGLh3UwtFLxBOW-_wuXCzc8-S-iC2_vDOuKOP1e3csU=" alt="" class="img-fluid">
+            </div>
+            <div class="col align-self-center">
+                <h4><a href="#" class="item-title">Belajar Dasar Pemrograman Web</a></h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis, consequatur!</p>
+                <p class="text-secondary">dibuat oleh Dianah</p>
+            </div>
+        </div>
+        <div class="row list-item mb-4">
+            <div class="col align-self-center">
+                <img src="https://media.istockphoto.com/photos/pond-at-waterline-picture-id1035285964?k=6&m=1035285964&s=612x612&w=0&h=yGLh3UwtFLxBOW-_wuXCzc8-S-iC2_vDOuKOP1e3csU=" alt="" class="img-fluid">
+            </div>
+            <div class="col align-self-center">
+                <h4><a href="#" class="item-title">Belajar Dasar Pemrograman Web</a></h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis, consequatur!</p>
+                <p class="text-secondary">dibuat oleh Dianah</p>
+            </div>
+        </div>
+        <div class="row list-item mb-4">
+            <div class="col align-self-center">
+                <img src="https://media.istockphoto.com/photos/pond-at-waterline-picture-id1035285964?k=6&m=1035285964&s=612x612&w=0&h=yGLh3UwtFLxBOW-_wuXCzc8-S-iC2_vDOuKOP1e3csU=" alt="" class="img-fluid">
+            </div>
+            <div class="col align-self-center">
+                <h4><a href="#" class="item-title">Belajar Dasar Pemrograman Web</a></h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis, consequatur!</p>
+                <p class="text-secondary">dibuat oleh Dianah</p>
+            </div>
+        </div>
+      </div>
+    </section>
       `;
   },
 
   async afterRender() {
-    // Fungsi ini akan dipanggil setelah render()
+    const listInfo = await FypDbSource.listInfo();
+    console.log(listInfo);
   },
 };
 
